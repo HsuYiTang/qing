0bd62a2cf536fd1ca5d694758349803a202e1938
MengChen0728 <123729937+MengChen0728@users.noreply.github.com>
2024-01-09 18:05:31 +0800

    ccc

--- JikanAPIViewer/JikanAPIViewer/Preview Content/AnimeItemView.swift
+++ JikanAPIViewer/JikanAPIViewer/Preview Content/AnimeItemView.swift
@@ -9,33 +9,43 @@ import Foundation
 import SwiftUI
 
 struct AnimeItemView: View{
+    let animatioEffect = Animation.linear(duration: 3)
     var item: Anime
     var showRank = true
+    @State var 位移 = 200.0
     var body: some View{
-        HStack{
-            rankView(rank: Int(item.rank ?? 0))
-                .opacity(showRank ? 1.0 : 0.0)
-            let url = URL(string: item.images?["jpg"]?.imageURL ?? "")!
-            AsyncImage(url: url,content:{ image in
-                image.resizable()
-                     .aspectRatio(contentMode: .fit)
-                     .frame(maxWidth: 80, maxHeight: 100)
-            },placeholder: {
-                ProgressView()
-            })
-            VStack(alignment: .leading) {
-                Text("\(item.titleJapanese ?? "")")
-                    .lineLimit(3)
-                    .font(.system(size: 18,weight: .heavy))
-                Text("類型：\(item.type ?? "")")
-                    .font(.system(size: 14))
-                Text(item.type == "Movie" ? "上映日期：\(item.aired?.from?.dateFormated() ?? "")" : "播放日期：\(item.aired?.from?.dateFormated() ?? "") ~ \(item.aired?.to?.dateFormated() ?? "To be continued")")
-                    .font(.system(size: 14))
-                    .lineLimit(3)
-                Text("評分：\(item.score ?? 0.0, specifier: "%.2f")")
-                    .font(.system(size: 14))
+            HStack{
+                rankView(rank: Int(item.rank ?? 0))
+                    .opacity(showRank ? 1.0 : 0.0)
+                let url = URL(string: item.images?["jpg"]?.imageURL ?? "")!
+                AsyncImage(url: url,content:{ image in
+                    image.resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 80, maxHeight: 100)
+                },placeholder: {
+                    ProgressView()
+                })
+                VStack(alignment: .leading) {
+                    Text("\(item.titleJapanese ?? "")")
+                        .offset(x: 位移, y: 0)
+                        .onAppear {
+                            withAnimation(animatioEffect.repeatForever(autoreverses: false)){
+                                位移 = 0.0
+                            }
+                        }
+                        //.clipped()
+                        //.lineLimit(1)
+                        .font(.system(size: 18,weight: .heavy))
+                    Text("類型：\(item.type ?? "")")
+                        .offset(x: 0.0)
+                        .font(.system(size: 14))
+                    Text(item.type == "Movie" ? "上映日期：\(item.aired?.from?.dateFormated() ?? "")" : "播放日期：\(item.aired?.from?.dateFormated() ?? "") ~ \(item.aired?.to?.dateFormated() ?? "To be continued")")
+                        .font(.system(size: 14))
+                        .lineLimit(3)
+                    Text("評分：\(item.score ?? 0.0, specifier: "%.2f")")
+                        .font(.system(size: 14))
+                }
             }
-        }
     }
 }
 struct AnimeItemView_Previews: PreviewProvider {
@@ -43,3 +53,20 @@ struct AnimeItemView_Previews: PreviewProvider {
         AnimeItemView(item: Anime(id: 123, url: "https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood", images: ["jpg": CoverImage(imageURL: "https://cdn.myanimelist.net/images/anime/1223/96541.jpg", smallImageURL: "https://cdn.myanimelist.net/images/anime/1223/96541.jpg", largeImageURL: "https://cdn.myanimelist.net/images/anime/1223/96541.jpg")], trailer: nil, title: "test", titleEnglish: "test", titleJapanese: "測試", titleSynonyms: nil, type: nil, source: nil, episodes: nil, status: nil, airing: nil, aired: nil, duration: nil, rating: nil, score: 5.0, scoredBy: nil, rank: 1.0, popularity: nil, members: nil, favorites: nil, synopsis: nil, background: nil, season: nil, year: nil, broadcast: nil, producers: nil, licensors: nil, studios: nil, genres: nil, explicitGenres: nil, themes: nil, demographics: nil))
     }
 }
+//struct 跑馬燈: View {
+//    @State var 位移: CGFloat = 450
+//    let 動畫效果 = Animation.linear(duration: 10.0)
+//    var body: some View {
+//        Text(訊息)
+//            .foregroundColor(.purple)
+//            .font(.system(size: 36))
+//            .frame(width: 800.0)
+//            .lineLimit(1)
+//            .offset(x: 位移, y: 0)
+//            .onAppear {
+//                withAnimation(動畫效果.repeatForever(autoreverses: false)) {
+//                    位移 = -450
+//                }
+//            }
+//    }
+//}
